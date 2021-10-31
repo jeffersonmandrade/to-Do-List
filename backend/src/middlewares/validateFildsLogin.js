61f5685612cac79c4c1e25a043c8fd766a9bad9c
Jefferson_Andrade <jffrsncrls@gmail.com>
2021-10-31 11:02:02 -0300

    feat: validate filds email and password

---
+++ backend/src/middlewares/validateFildsLogin.js
@@ -0,0 +1,19 @@
+const rescue  = require('express-rescue');
+const Joi = require('joi');
+
+
+
+
+
+module.exports = rescue(async(req, res,next) => {
+const{ email, password } = req.body
+const {error} = Joi.object({
+email: Joi.string().email().required(),
+password:Joi.string().min(8).required(),
+}).validate({email, password});
+
+if(error){
+  return next(error)
+}
+next()
+})
