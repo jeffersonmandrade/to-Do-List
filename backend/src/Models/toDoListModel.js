4f4f092ed5b2c012aa68297ec302b8b0eb16c0f7
Jefferson_Andrade <jffrsncrls@gmail.com>
2021-11-03 19:59:16 -0300

    feat: update task creado

--- backend/src/Models/toDoListModel.js
+++ backend/src/Models/toDoListModel.js
@@ -1,3 +1,4 @@
+const { ObjectId } = require('mongodb');
 const connection = require('./connection');
 
 const createTask = async(body) => {
@@ -13,7 +14,23 @@ const findAllUser = async(idUser) => {
   return findAll;
 }
 
+const updateTask = async(id, body) => {
+  if (!ObjectId.isValid(id)) {
+    return null;
+  }
+const db = await connection();
+const { value } = db.collection('toDolist').findOneAndUpdate(
+  {_id: ObjectId(id)},
+  {$set: body},
+  { retrunDocument: "after"},
+
+)
+return value
+
+}
+
 module.exports = {
   createTask,
   findAllUser,
+  updateTask,
 }
