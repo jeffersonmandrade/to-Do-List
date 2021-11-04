aef0f390a3d461b8d36f7e6bd7860531f48b5ced
Jefferson_Andrade <jffrsncrls@gmail.com>
2021-11-04 00:14:27 -0300

    feat: deleteTask implementada  no controller.

--- backend/src/Controllers/toDoListController.js
+++ backend/src/Controllers/toDoListController.js
@@ -1,37 +1,39 @@
-const toDoListService = require('../Services/toDoListService')
+const toDoListService = require("../Services/toDoListService");
 
-const createTask = async(req,res,next) => {
-const{id, email } = req.user;
-const {task , status} = req.body;
-const create = await toDoListService.createTask(email,id,task,status).catch(e => console.log(e))
-res.status(200).json(create);
-}
+const createTask = async (req, res, next) => {
+  const { id, email } = req.user;
+  const { task, status } = req.body;
+  const create = await toDoListService
+    .createTask(email, id, task, status)
+    .catch((e) => console.log(e));
+  res.status(200).json(create);
+};
 
-const findAllUser  = async(req,res,next) => {
-const {id} = req.user;
-const findAll = await toDoListService.findAllUser(id);
-res.status(200).json(findAll);
-return findAll
+const findAllUser = async (req, res, next) => {
+  const { id } = req.user;
+  const findAll = await toDoListService.findAllUser(id);
+  res.status(200).json(findAll);
+  return findAll;
+};
 
-}
+const updateTask = async (req, res, next) => {
+  const { id } = req.user;
+  const { email, task, status, _id } = req.body;
+  const update = await toDoListService
+    .updateTask(email, id, task, status, _id)
+    .catch((erro) => console.log(erro));
+  res.status(200).json(update);
+};
 
-const updateTask = async(req,res,next) => {
-const {id} = req.user;
-  const {email,task,status,_id} = req.body
-  const update = await toDoListService.updateTask(email,id,task,status,_id).catch(erro => console.log(erro))
-  res.status(200).json(update)
-}
-
-const deleteTask = async(req,res,next) => {
-  const{_id: id} = req.body
-  // const delete = await
-}
-
-
+const deleteTask = async (req, res, next) => {
+  const { _id: id } = req.body;
+  const deleteData = await toDoListService.deleteTask(id);
+  res.status(200).json(deleteData);
+};
 
 module.exports = {
   createTask,
   findAllUser,
   updateTask,
-  deleteTask
-}
+  deleteTask,
+};
